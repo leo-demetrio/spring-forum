0e8d6eb9e6f009cc86e66cfbf660ef57369bef8d
leo-demetrio <leocdemetrio@gmail.com>
2022-10-08 11:17:52 -0300

    Feat: TopicDto and method convert

---
+++ src/main/java/com/leodemetrio/forum/TopicDto.java
@@ -0,0 +1,42 @@
+package com.leodemetrio.forum;
+
+import com.leodemetrio.forum.model.Topic;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class TopicDto {
+
+    private Long id;
+    private String title;
+    private String message;
+    private LocalDateTime dataCreation = LocalDateTime.now();
+
+    public TopicDto(Topic topic){
+        this.id = topic.getId();
+        this.title = topic.getTitle();
+        this.message = topic.getMessage();
+        this.dataCreation = topic.getDataCreation();
+    }
+    public static List<TopicDto> convert(List<Topic> topic){
+        return topic.stream().map(TopicDto::new).collect(Collectors.toList());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public LocalDateTime getDataCreation() {
+        return dataCreation;
+    }
+}
--- src/main/java/com/leodemetrio/forum/controller/TopicsController.java
+++ src/main/java/com/leodemetrio/forum/controller/TopicsController.java
@@ -1,5 +1,6 @@
 package com.leodemetrio.forum.controller;
 
+import com.leodemetrio.forum.TopicDto;
 import com.leodemetrio.forum.model.Course;
 import com.leodemetrio.forum.model.Topic;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,8 +14,9 @@ import java.util.List;
 @RequestMapping("/topics")
 public class TopicsController {
 
+
     @GetMapping
-    public List<Topic> listAll(){
-        return Arrays.asList(new Topic("Duvida","Duvida Spring", new Course("Spring", "Programing")));
+    public List<TopicDto> listAll(){
+        return TopicDto.convert(Arrays.asList(new Topic("Dúvida", "Spring", new Course("Spring", "Programing"))));
     }
 }
